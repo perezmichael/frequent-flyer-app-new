3ada6b07d528572ac7e0d7e91d1f702ce337248a
Mike <hi@mikeperezdigital.com>
2024-05-18 17:09:50 -0700

    GraphQL

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = () => ({
+    graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
